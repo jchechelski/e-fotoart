1b32fa1635eb8b727ba8582d025abb57d81b6e82
Jarek <chechelskiwsei@gmail.com>
2017-03-18 02:23:51 +0100

    poprawiona animacja + loop sass

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,20 @@
-
-
-function randomRealizacje() {
-  
-}
-
-//randomRealizacje();
-
-
-function showRealizacje() {
-    var realizacje = document.getElementById('container-realizacje'),
+   var realizacje = document.getElementById('container-realizacje'),
     realizacjeLists = realizacje.children,
     realizacjeLength = realizacjeLists.length;
 
-var tab = [];
+
     
+function showRealizacje() {
+ 
     
     // function to check exists class
-    
+    var tab = [];
 
 for(var i=0; i<realizacjeLength; i++) {
-    los = Math.floor(1+Math.random()*6);
-
+    var los = Math.floor(1+Math.random()*6);
+     
     if(tab.indexOf(los) == -1) {
         tab.push(los);
-        
        realizacjeLists[i].innerHTML = '<img src="img/realizacje/'+los+'.png" class="img-responsive show-anime" alt="">'; 
     } else {
         i--;
@@ -32,37 +23,22 @@ for(var i=0; i<realizacjeLength; i++) {
 }
 
 function hideRealizacje() {
-      var realizacje = document.getElementById('container-realizacje'),
-    realizacjeLists = realizacje.children,
-    realizacjeLength = realizacjeLists.length;
+    for(var j=0; j<realizacjeLength; j++) {
 
-var tab = [];
-    
-    
- 
-
-for(var i=0; i<realizacjeLength; i++) {
-    los = Math.floor(1+Math.random()*6);
-
-    if(tab.indexOf(los) == -1) {
-        tab.push(los);
-
-       realizacjeLists[i].innerHTML = '<img src="img/realizacje/'+los+'.png" class="img-responsive hide-anime" alt="">'; 
-    } else {
-        i--;
-    }   
+    realizacjeLists[j].children[0].classList.remove("show-anime");
+    realizacjeLists[j].children[0].classList.add("hide-anime");
 }  
 } 
 showRealizacje();
 setInterval(function() {
     
     setTimeout(hideRealizacje, 3000);
-    setTimeout(showRealizacje, 6000);
+    setTimeout(showRealizacje, 4300);
     
-},15000)
+},10000)
 
-//setInterval(hideRealizacje, 4000);
-//setInterval(showRealizacje, 5000);
+//setInterval(hideRealizacje, 3000);
+//setInterval(showRealizacje, 6000);
 
 
 
